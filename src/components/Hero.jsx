0ef53c56f72bbbbaf5b6a41b61b0a8a55edb3c42
RedwanHossainMurdochUni <161429153+RedwanHossainMurdochUni@users.noreply.github.com>
2025-06-07 11:30:09 +0800

    Update Hero.jsx update pic

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/TripPic.jpg";
+import profilePic from "../assets/RedsSuitePic.jpg";
 import { motion } from "framer-motion";
 
 const Hero = () => {
